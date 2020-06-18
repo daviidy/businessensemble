f0d9aeba3ef20e2fbedcc5e24f24f068937ae3b7
daviidy <yaodavidarmel@gmail.com>
2020-06-18 09:44:13 +0000

    adding AnnonceController and model Annonce

---
+++ app/Http/Controllers/AnnonceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Annonce;
+use Illuminate\Http\Request;
+
+class AnnonceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Annonce  $annonce
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Annonce $annonce)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Annonce  $annonce
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Annonce $annonce)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Annonce  $annonce
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Annonce $annonce)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Annonce  $annonce
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Annonce $annonce)
+    {
+        //
+    }
+}
