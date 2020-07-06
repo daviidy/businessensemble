3b317538e12da9e503085dbabfcd202025cc1b89
aouman <37551703+aouman@users.noreply.github.com>
2020-07-06 16:20:48 +0000

    champ selecte

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -251,6 +251,243 @@ form .inner-form .input-field.second-wrap{width:100%;margin-bottom:30px;}
 form .inner-form .input-field.second-wrap input{border:1px solid rgba(255, 255, 255, 0.3);}
 form .inner-form .input-field.third-wrap{margin-bottom:0;width:100%;}
 }
+
+/*=====  End of Choices  ======*/
+* {
+  box-sizing: border-box;
+}
+
+.s003 {
+  /*min-height: 100vh;*/
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-pack: center;
+      justify-content: center;
+  -ms-flex-align: center;
+      align-items: center;
+  font-family: 'Poppins', sans-serif;
+  /*background-color: #a598ee;*/
+  /*background-size: cover;
+  background-position: center center;*/
+  padding: 15px;
+}
+
+.s003 form {
+  width: 100%;
+  max-width: 790px;
+  margin-bottom: 0;
+}
+
+.s003 form .inner-form {
+  background: #fff;
+  display: -ms-flexbox;
+  display: flex;
+  width: 100%;
+  -ms-flex-pack: justify;
+      justify-content: space-between;
+  -ms-flex-align: center;
+      align-items: center;
+  box-shadow: 0px 8px 20px 0px rgba(0, 0, 0, 0.15);
+  border-radius: 3px;
+}
+
+.s003 form .inner-form .input-field {
+  height: 68px;
+}
+
+.s003 form .inner-form .input-field input {
+  height: 100%;
+  background: transparent;
+  border: 0;
+  display: block;
+  width: 100%;
+  padding: 10px 32px;
+  font-size: 16px;
+  color: #555;
+}
+
+.s003 form .inner-form .input-field input.placeholder {
+  color: #888;
+  font-size: 16px;
+}
+
+.s003 form .inner-form .input-field input:-moz-placeholder {
+  color: #888;
+  font-size: 16px;
+}
+
+.s003 form .inner-form .input-field input::-webkit-input-placeholder {
+  color: #888;
+  font-size: 16px;
+}
+
+.s003 form .inner-form .input-field input:hover, .s003 form .inner-form .input-field input:focus {
+  box-shadow: none;
+  outline: 0;
+  border-color: #fff;
+}
+
+.s003 form .inner-form .input-field.first-wrap {
+  width: 200px;
+  border-right: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+.s003 form .inner-form .input-field.first-wrap .choices__inner {
+  background: transparent;
+  border-radius: 0;
+  border: 0;
+  height: 100%;
+  color: #fff;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+      align-items: center;
+  padding: 10px 30px;
+}
+
+.s003 form .inner-form .input-field.first-wrap .choices__inner .choices__list.choices__list--single {
+  display: -ms-flexbox;
+  display: flex;
+  padding: 0;
+  -ms-flex-align: center;
+      align-items: center;
+  height: 100%;
+}
+
+.s003 form .inner-form .input-field.first-wrap .choices__inner .choices__item.choices__item--selectable.choices__placeholder {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+      align-items: center;
+  height: 100%;
+  opacity: 1;
+  color: #888;
+}
+
+.s003 form .inner-form .input-field.first-wrap .choices__inner .choices__list--single .choices__item {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+      align-items: center;
+  height: 100%;
+  color: #555;
+}
+
+.s003 form .inner-form .input-field.first-wrap .choices[data-type*="select-one"]:after {
+  right: 30px;
+  border-color: #e5e5e5 transparent transparent transparent;
+}
+
+.s003 form .inner-form .input-field.first-wrap .choices__list.choices__list--dropdown {
+  border: 0;
+  background: #fff;
+  padding: 20px 30px;
+  margin-top: 2px;
+  border-radius: 4px;
+  box-shadow: 0px 8px 20px 0px rgba(0, 0, 0, 0.15);
+}
+
+.s003 form .inner-form .input-field.first-wrap .choices__list.choices__list--dropdown .choices__item--selectable {
+  padding-right: 0;
+}
+
+.s003 form .inner-form .input-field.first-wrap .choices__list--dropdown .choices__item--selectable.is-highlighted {
+  background: #fff;
+  color: #63c76a;
+}
+
+.s003 form .inner-form .input-field.first-wrap .choices__list--dropdown .choices__item {
+  color: #555;
+  min-height: 24px;
+}
+
+.s003 form .inner-form .input-field.second-wrap {
+  -ms-flex-positive: 1;
+      flex-grow: 1;
+}
+
+.s003 form .inner-form .input-field.third-wrap {
+  width: 74px;
+}
+
+.s003 form .inner-form .input-field.third-wrap .btn-search {
+  height: 100%;
+  width: 100%;
+  white-space: nowrap;
+  color: #fff;
+  border: 0;
+  cursor: pointer;
+  background: #63c76a;
+  transition: all .2s ease-out, color .2s ease-out;
+}
+
+.s003 form .inner-form .input-field.third-wrap .btn-search svg {
+  width: 16px;
+}
+
+.s003 form .inner-form .input-field.third-wrap .btn-search:hover {
+  background: #50c058;
+}
+
+.s003 form .inner-form .input-field.third-wrap .btn-search:focus {
+  outline: 0;
+  box-shadow: none;
+}
+
+@media screen and (max-width: 992px) {
+  .s003 form .inner-form .input-field {
+    height: 50px;
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .s003 form .inner-form {
+    -ms-flex-wrap: wrap;
+        flex-wrap: wrap;
+    padding: 20px;
+  }
+  .s003 form .inner-form .input-field {
+    margin-bottom: 20px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  }
+  .s003 form .inner-form .input-field input {
+    padding: 10px 15px;
+  }
+  .s003 form .inner-form .input-field.first-wrap {
+    width: 100%;
+    border-right: 0;
+  }
+  .s003 form .inner-form .input-field.first-wrap .choices__inner {
+    padding: 10px 15px;
+  }
+  .s003 form .inner-form .input-field.first-wrap .choices[data-type*="select-one"]:after {
+    right: 11.5px;
+    border-color: #e5e5e5 transparent transparent transparent;
+  }
+  .s003 form .inner-form .input-field.second-wrap {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+  .s003 form .inner-form .input-field.second-wrap input {
+    border: 1px solid rgba(255, 255, 255, 0.3);
+  }
+  .s003 form .inner-form .input-field.third-wrap {
+    margin-bottom: 0;
+    width: 100%;
+  }
+
+  .input-select select{
+	
+    padding: 5%;
+    
+	}
+}
+
+.input-select select{
+	width: 100%;
+    padding: 10%;
+    border: none;
+}
 </style>
 
 
@@ -258,79 +495,36 @@ form .inner-form .input-field.third-wrap{margin-bottom:0;width:100%;}
 
 <div style="margin-top: 2rem;" class="ss-page-title">Liste des utilisateurs</div>
 
-<div class="row">
-
-    <form style="margin: auto;" action="/searchUsers" enctype="multipart/form-data" method="post">
+<div class="s003">
+      <form action="/searchUsers" enctype="multipart/form-data" method="post">
         <div class="inner-form">
-            <div class="input-field first-wrap">
-                <div class="input-select">
-                    <div class="choices" role="listbox" data-type="select-one" tabindex="0" aria-haspopup="true" aria-expanded="false" dir="ltr" aria-activedescendant="choices-choices-single-defaul-m6-item-choice-2">
-                        <div class="choices__inner">
-                        	<select data-trigger="" name="choices-single-defaul" class="choices__input is-hidden" tabindex="-1" style="display:none;" aria-hidden="true" data-choice="active">
-                                <option id="text_tag_option" value="Category" selected="">Category</option>
-                            </select>
-                            <div class="choices__list choices__list--single">
-                                <div class="choices__item choices__item--selectable choices__placeholder" data-item="" data-id="1"  id="choices__placeholder">
-                                	<span>Category</span>
-                                   
-                                </div>
-                            </div>
-                        </div>
-                        <div class="choices__list choices__list--dropdown" aria-expanded="false">
-                            <div class="choices__list" dir="ltr" role="listbox">
-                                <div class="choices__item choices__item--choice choices__item--selectable choices__placeholder is-highlighted" data-select-text="" data-choice="" data-id="2" data-value="Category" data-choice-selectable=""
-                                  id="choices-choices-single-defaul-m6-item-choice-2" role="option" aria-selected="true">
-                                    Category
-                                </div>
-                                <div class="choices__item choices__item--choice choices__item--selectable" data-select-text="" data-choice="" data-id="1" data-value="Accessories" data-choice-selectable=""
-                                  id="choices-choices-single-defaul-m6-item-choice-1 " role="option">
-                                    Accessories
-                                </div>
-                                <div class="choices__item choices__item--choice choices__item--selectable" data-select-text="" data-choice="" data-id="3" data-value="Clothing" data-choice-selectable="" id="choices-choices-single-defaul-m6-item-choice-3"
-                                  role="option">
-                                    Clothing
-                                </div>
-                                <div class="choices__item choices__item--choice choices__item--selectable" data-select-text="" data-choice="" data-id="4" data-value="Footwear" data-choice-selectable="" id="choices-choices-single-defaul-m6-item-choice-4"
-                                  role="option">
-                                    Footwear
-                                </div>
-                                <div class="choices__item choices__item--choice choices__item--selectable" data-select-text="" data-choice="" data-id="5" data-value="Ladies" data-choice-selectable="" id="choices-choices-single-defaul-m6-item-choice-5"
-                                  role="option">
-                                    Ladies
-                                </div>
-                                <div class="choices__item choices__item--choice choices__item--selectable" data-select-text="" data-choice="" data-id="6" data-value="Men" data-choice-selectable="" id="choices-choices-single-defaul-m6-item-choice-6"
-                                  role="option">
-                                    Men
-                                </div>
-                                <div class="choices__item choices__item--choice choices__item--selectable" data-select-text="" data-choice="" data-id="7" data-value="New Arrivals" data-choice-selectable=""
-                                  id="choices-choices-single-defaul-m6-item-choice-7" role="option">
-                                    New Arrivals
-                                </div>
-                                <div class="choices__item choices__item--choice choices__item--selectable" data-select-text="" data-choice="" data-id="8" data-value="Sale" data-choice-selectable="" id="choices-choices-single-defaul-m6-item-choice-8"
-                                  role="option">
-                                    Sale
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          <div class="input-field first-wrap">
+            <div class="input-select">
+              <select data-trigger="" name="choices-single-defaul">
+                <option placeholder="">Category</option>
+                <option>New Arrivals</option>
+                <option>Sale</option>
+                <option>Ladies</option>
+                <option>Men</option>
+                <option>Clothing</option>
+                <option>Footwear</option>
+                <option>Accessories</option>
+              </select>
             </div>
-            <div class="input-field second-wrap">
-                <input id="search" type="text" placeholder="Entrer un nom d'utilisateur">
-            </div>
-            <div class="input-field third-wrap">
-                <button class="btn-search" type="button">
-                    <svg class="svg-inline--fa fa-search fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path fill="currentColor"
-                          d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z">
-                        </path>
-                    </svg>
-                </button>
-            </div>
+          </div>
+          <div class="input-field second-wrap">
+            <input id="search" type="text" placeholder="Enter Keywords?" />
+          </div>
+          <div class="input-field third-wrap">
+            <button class="btn-search" type="button">
+              <svg class="svg-inline--fa fa-search fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+              </svg>
+            </button>
+          </div>
         </div>
-    </form>
-
-</div>
+      </form>
+    </div>
 <div style="margin-bottom: 2rem;justify-content: center;" class="styles__CategoryButtons-vgii8s-3 hqqZXZ">
 <div style="padding: 2rem;" class="row">
 
@@ -358,6 +552,7 @@ form .inner-form .input-field.third-wrap{margin-bottom:0;width:100%;}
 </div>
 </div>
 <script>
+	/*
 $(document).ready(function(){
     $(".choices__list--single").click(function(){
         $(".choices__list--dropdown").toggle();
@@ -371,7 +566,7 @@ $(document).ready(function(){
 	    $(".choices__list--dropdown").hide();
 	    return false;
 	});
-});
+});*/
 </script>
 
 @endsection
