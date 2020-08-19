2e58e3c0231fe660f5c518d00ae7638855247b46
aouman <37551703+aouman@users.noreply.github.com>
2020-08-19 09:56:10 +0000

    slider font-familly

--- resources/views/includes/slide.blade.php
+++ resources/views/includes/slide.blade.php
@@ -3,11 +3,15 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <style media="screen">
+    body{
+      font-family: 'Montserrat', sans-serif;
+    }
   .main-container {
 padding: 10px 15px;
 }
